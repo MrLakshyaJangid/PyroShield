b3566c36a720ec045ace4967b69ea5d36d637d37
MrLakshyaJangid <mr.lakshya08@gmail.com>
2025-04-18 17:39:09 +0530

    Delete backend directory

--- backend/routes/userReportRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getUserReports, addReport } = require("../controllers/userReportController");
-const authenticateToken = require("../middleware/authMiddleware");
-
-router.get("/", authenticateToken, getUserReports);
-router.post("/", authenticateToken, addReport);
-
-module.exports = router;
